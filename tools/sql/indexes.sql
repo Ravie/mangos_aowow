0fe6a75639f14476e2a8d109602167733f7ad575
Ravie <ravijkee@yandex.ru>
2015-01-02 03:21:43 +0300

    [72] Fix indexes

--- tools/sql/indexes.sql
+++ tools/sql/indexes.sql
@@ -18,9 +18,9 @@ CREATE INDEX `aowow_item` ON `prospecting_loot_template`  (`item`);
 CREATE INDEX `aowow_item` ON `skinning_loot_template`     (`item`);
 CREATE INDEX `aowow_item` ON `reference_loot_template`    (`item`);
 
-CREATE INDEX `aowow_lootid`         ON `creature_template` (`lootid`);
-CREATE INDEX `aowow_skinloot`       ON `creature_template` (`skinloot`);
-CREATE INDEX `aowow_pickpocketloot` ON `creature_template` (`pickpocketloot`);
-CREATE INDEX `aowow_faction_A`      ON `creature_template` (`faction_A`);
+CREATE INDEX `aowow_lootid`         ON `creature_template` (`LootId`);
+CREATE INDEX `aowow_skinloot`       ON `creature_template` (`SkinningLootId`);
+CREATE INDEX `aowow_pickpocketloot` ON `creature_template` (`PickpocketLootId`);
+CREATE INDEX `aowow_faction_A`      ON `creature_template` (`FactionAlliance`);
 
 CREATE INDEX `aowow_faction`        ON `item_template`     (`RequiredReputationFaction`);
